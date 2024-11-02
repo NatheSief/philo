3cf08bca2341ab0d7b64fcb4e3e10b899710e094
xlebecq <xlebecq@student.42.fr>
2024-11-02 15:16:17 +0100

    Sat Nov  2 03:16:17 PM CET 2024

---
+++ philo/utils.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 09:33:12 by xlebecq           #+#    #+#             */
+/*   Updated: 2024/11/02 09:41:16 by xlebecq          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	ft_error_msg(char *msg)
+{
+	if (msg = "invalid number of arguments")
+		write(2, "Error, invalid number of arguments", 34);
+}
