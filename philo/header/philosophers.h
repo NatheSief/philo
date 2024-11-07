724ba5af928849f23c8dc5d4c124e61cc83d299d
xlebecq <xlebecq@student.42.fr>
2024-11-07 18:50:43 +0100

    Thu Nov  7 06:50:43 PM CET 2024

--- philo/header/philosophers.h
+++ philo/header/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:22:51 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/11/07 08:48:56 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/11/07 11:25:37 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,33 @@
 # include <stdio.h>
 # include <stdint.h>
 
+struct s_sphilosophers;
+
 typedef struct	s_dining_cfg
 {
-	int		number_of_philosophers;
-	uint64_t	time_to_die;
-	uint64_t	time_to_eat;
-	uint64_t	time_to_sleep;
-	int		nb_of_times_each_philosopher_must_eat;
+	struct s_philosophers	*philosophers;
+	int			number_of_philosophers;
+	uint64_t		time_to_die;
+	uint64_t		time_to_eat;
+	uint64_t		time_to_sleep;
+	int			nb_of_times_each_philosopher_must_eat;
+	pthread_mutex_t		*m_forks;
+	pthread_mutex_t		m_write;
+	pthread_mutex_t		m_dead;
 }	t_dining_cfg;
 
 typedef struct	s_philosophers
 {
-	int	id;
+	t_dining_cfg	*dining_cfg;
+	int		id;
+	uint64_t	time_since_eat;
+	int		eating;
+	uint64_t	time_limit;
+	int		eat_count;
+	int		l_fork;
+	int		r_fork;
+	pthread_mutex_t	m_mutex;
+	pthread_mutex_t	m_eating;
 }	t_philosophers;
 
 void	ft_error_msg(const char *msg);
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,13 @@
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 09:28:06 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/11/07 08:49:58 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/11/07 18:45:34 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	get_args(t_dining_cfg *dining_cfg, int argc, const char **argv)
+void	ft_get_args(t_dining_cfg *dining_cfg, int argc, const char **argv)
 {
 	if (argc < 5 || argc > 6)
 		ft_error_msg("Error : invalid number of arguments.\n");
@@ -32,21 +32,38 @@ void	get_args(t_dining_cfg *dining_cfg, int argc, const char **argv)
 		|| dining_cfg->time_to_sleep < 60)
 		ft_error_msg("Error : time_to_die, time_to_eat, and\
  time_to_sleep must be at least 60ms.\n");
-	
+}
+
+void	ft_init_philosophers(t_dining_cfg *dining_cfg)
+{
+	int	i;
+
+	i = 0;
+	while (i < 5)
+	{
+		dining_cfg->philosophers[i].id = i;
+		dining_cfg->philosophers[i].l_fork = i;
+		dining_cfg->philosophers[i].r_fork = (i + 1);
+		dining_cfg->philosophers[i].eat_count = 0;
+		printf("%d\n", dining_cfg->philosophers[i].id);
+		i++;
+	}
 }
 
 int	main(int argc, const char **argv)
 {
 	t_dining_cfg	dining_cfg;
-	t_philosophers	*philosophers;
 
-	get_args(&dining_cfg, argc, argv);
-	philosophers = (t_philosophers *)malloc(sizeof(t_philosophers *) * dining_cfg.number_of_philosophers);
-	philosophers = (t_philosophers *)malloc(sizeof(*philosophers) * dining_cfg.number_of_philosophers);
-/*	printf("number_of_philosophers = %d\ntime_to_die = %lu\n\
+	dining_cfg.philosophers = NULL;
+	ft_get_args(&dining_cfg, argc, argv);
+	dining_cfg.philosophers = (t_philosophers *)malloc(sizeof(*(dining_cfg.philosophers)) * dining_cfg.number_of_philosophers);
+	if (!dining_cfg.philosophers)
+		ft_error_msg("Error: Memory allocation failed for philosophers.\n");
+	ft_init_philosophers(&dining_cfg);
+	printf("number_of_philosophers = %d\ntime_to_die = %lu\n\
 time_to_eat = %lu\ntime_to_sleep = %lu\nnb_of_times_each_philosopher_must_eat =\
  %d\n", dining_cfg.number_of_philosophers, dining_cfg.time_to_die, \
 dining_cfg.time_to_eat, dining_cfg.time_to_sleep, \
-dining_cfg.nb_of_times_each_philosopher_must_eat);*/
+dining_cfg.nb_of_times_each_philosopher_must_eat);
 	return (0);
 }
