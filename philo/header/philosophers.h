5eb2062764e0f77178af41ff20b6aa2bd61bdc8d
xlebecq <xlebecq@student.42.fr>
2024-11-21 19:45:52 +0100

    &(date)

--- philo/header/philosophers.h
+++ philo/header/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: xlebecq <xlebecq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:22:51 by xlebecq           #+#    #+#             */
-/*   Updated: 2024/11/19 19:20:04 by xlebecq          ###   ########.fr       */
+/*   Updated: 2024/11/21 01:40:46 by xlebecq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ typedef struct s_cfg
 	uint64_t				time_to_eat;
 	uint64_t				time_to_sleep;
 	uint64_t				time;
-	int						meals_required;
+	size_t					meals_required;
 	pthread_mutex_t			*forks_mutex;
 	pthread_mutex_t			display_mutex;
 	pthread_mutex_t			dead_mutex;
@@ -72,24 +72,32 @@ void		*ft_start_routine(void *s);
 /*							   	ERRORS_MESSAGES                               */
 /*----------------------------------------------------------------------------*/
 
-# define ERROR_ARGS						"Error: Invalid number of arguments.\n"
-# define ERROR_NB_PHILO	\
-	"Error: Nb of philosophers must beetween 1 and 200.\n"
-# define ERROR_LIMIT_ARGS \
-	"Error: time_to_die, time_to_eat, and time_to_sleep must be at least 60ms.\n"
-# define ERROR_MALLOC_PHILO 			"Error: Memory allocation failed for philosophers.\n"
-# define ERROR_MALLOC_MUTEX			 	"Error: Memory allocation failed for forks_mutex.\n"
-# define ERROR_FORKS_MUTEX				"Error: Failed to initialize forks_mutex.\n"
-# define ERROR_MUTEX_MUTEX				"Error: Failed to initialize mutex mutex.\n"
-# define ERROR_EATING_MUTEX				"Error: Failed to initialize eating_mutex.\n"
-# define ERROR_EATING_MUTEX_LOCK		"Error: Failed to lock eating_mutex.\n"
-# define ERROR_DISPLAY_MUTEX		 	"Error: Failed to initialize display_mutex.\n"
-# define ERROR_DEAD_MUTEX			 	"Error: Failed to initialize dead_mutex.\n"
-# define ERROR_DEAD_MUTEX_LOCK			"Error: Failed to lock dead_mutex.\n"
-# define ERROR_NEGATIVE_ARGS			"Error: Argument must be a positive number.\n"
-# define ERROR_OVERFLOW					"Error: Argument exceeds maximum integer value.\n"
-# define ERROR_INVALID_CHARACTERS		"Error: Argument contains invalid characters.\n"
-# define ERROR_MEALS_MONITOR_CREATE		"Error: Failed to create thread ft_meals_monitor.\n"
-# define ERROR_START_ROUTINE_CREATE		"Error: Failed to create thread ft_start_routine.\n"
+# define ERROR_ARGS 				"Error: Invalid number of arguments.\n"
+# define ERROR_NB_PHILO 			"Error: Nb of philosophers must beetween 1 \
+and 200.\n"
+# define ERROR_LIMIT_ARGS			"Error: time_to_die, time_to_eat, and \
+time_to_sleep must be at least 60ms.\n"
+# define ERROR_MALLOC_PHILO 		"Error: Memory allocation failed for \
+philosophers.\n"
+# define ERROR_MALLOC_MUTEX			"Error: Memory allocation failed for forks_\
+mutex.\n"
+# define ERROR_FORKS_MUTEX			"Error: Failed to initialize forks_mutex.\n"
+# define ERROR_MUTEX_MUTEX			"Error: Failed to initialize mutex mutex.\n"
+# define ERROR_EATING_MUTEX			"Error: Failed to initialize eating_mutex.\n"
+# define ERROR_EATING_MUTEX_LOCK	"Error: Failed to lock eating_mutex.\n"
+# define ERROR_DISPLAY_MUTEX		"Error: Failed to initialize display_\
+mutex.\n"
+# define ERROR_DEAD_MUTEX			"Error: Failed to initialize dead_mutex.\n"
+# define ERROR_DEAD_MUTEX_LOCK		"Error: Failed to lock dead_mutex.\n"
+# define ERROR_NEGATIVE_ARGS		"Error: Argument must be a positive \
+number.\n"
+# define ERROR_OVERFLOW				"Error: Argument exceeds maximum integer \
+value.\n"
+# define ERROR_INVALID_CHARACTERS	"Error: Argument contains invalid \
+characters.\n"
+# define ERROR_MEALS_MONITOR_CREATE	"Error: Failed to create thread ft_meals_\
+monitor.\n"
+# define ERROR_START_ROUTINE_CREATE	"Error: Failed to create thread ft_start_\
+routine.\n"
 
 #endif
